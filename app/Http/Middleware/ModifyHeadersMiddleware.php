400f3d28989aeb7fae4630f35f7a1e0448b1b37c
samsul bahari <samsul.bahari23@gmail.com>
2023-01-30 10:46:49 +0700

    cors

--- app/Http/Middleware/ModifyHeadersMiddleware.php
+++ app/Http/Middleware/ModifyHeadersMiddleware.php
@@ -19,7 +19,7 @@ class ModifyHeadersMiddleware
         return $next($request)
         ->header('Access-Control-Allow-Origin', '*')
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorizations');
+        ->header('Access-Control-Allow-Headers', 'Origin, Accept,X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization');
 
     }
 }
